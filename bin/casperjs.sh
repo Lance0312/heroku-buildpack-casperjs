ab2e1c5b6a8cfccb07520ffd8574b9a50dd5f8bd
Sean Brewer <seabre986@gmail.com>
2014-09-21 04:28:39 -0400

    Do not use ${}.

--- bin/casperjs.sh
+++ bin/casperjs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Add phantomjs/casperjs paths ..."
-export PATH="${PATH}:/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/app/vendor/phantomjs/lib"
+export PATH="$PATH:/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/vendor/phantomjs/lib"
 
