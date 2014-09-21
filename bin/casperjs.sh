9ea3ff53fb2f9434f19ffcb0635c4c5b3fee1d8d
Sean Brewer <seabre986@gmail.com>
2014-09-21 04:33:25 -0400

    Remove shebang.

--- bin/casperjs.sh
+++ bin/casperjs.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 echo "Add phantomjs/casperjs paths ..."
 export PATH="$PATH:/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/vendor/phantomjs/lib"
